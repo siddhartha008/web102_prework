fa48e5d2bac674526db90257e80a0ae2f8c1804c
Siddhartha Mishra <siddharthamishrak@gmail.com>
2025-04-19 16:41:33 -0500

    added: UI cards displayed

--- index.js
+++ index.js
@@ -28,28 +28,27 @@ const gamesContainer = document.getElementById("games-container");
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
 
-    // loop over each item in the data
-
-
+    // loop over each item in the data 
+    for (let i = 0; i < games.length; i++) { 
         // create a new div element, which will become the game card
-
-
+        let createdDiv = document.createElement("div"); 
         // add the class game-card to the list
-
-
+        createdDiv.classList.add("game-card"); 
         // set the inner HTML using a template literal to display some info 
-        // about each game
-        // TIP: if your images are not displaying, make sure there is space
-        // between the end of the src attribute and the end of the tag ("/>")
-
-
+        createdDiv.innerHTML = `
+            <img src="${games[i].img}" class="game-img" />
+            <h3>${games[i].name}</h3>
+            <p>${games[i].description}</p>
+            <p>Backers: ${games[i].backers}</p>
+        `; 
         // append the game to the games-container
-
+        gamesContainer.appendChild(createdDiv);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
